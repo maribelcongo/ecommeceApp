0481a99b2fde561846e33d41c66050a8eafa449d
maribel congo <congoceleste1@gmail.com>
2024-11-26 22:55:08 -0300

    manejo del detalle de producto

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,36 +1,5 @@
-// import React, { createContext, useContext, useState, useEffect } from "react";
-// import { auth } from "../../firebase";
-// import { onAuthStateChanged, signOut } from "firebase/auth"; // Asegúrate de importar signOut
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//     const [currentUser, setCurrentUser] = useState(null);
-
-//     useEffect(() => {
-//         const unsubscribe = onAuthStateChanged(auth, (user) => {
-//             setCurrentUser(user);
-//         });
-//         return () => unsubscribe();
-//     }, []);
-
-//     const logout = async () => {
-//         await signOut(auth); // Cierra sesión
-//         setCurrentUser(null); // Actualiza el estado del usuario
-//     };
-
-//     return (
-//         <AuthContext.Provider value={{ currentUser, logout }}>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
-
-// export const useAuth = () => {
-//     return useContext(AuthContext);
-// };
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { auth } from "../../firebase"; // Asegúrate de que tu archivo de configuración de Firebase esté correctamente importado
+import { auth } from "../../firebase";
 import {
   onAuthStateChanged,
   signOut,
